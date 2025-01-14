24ea6deac506295aa783cc789306d40a828a9556
RabaTamas <tomi.dani15@gmail.com>
2025-01-14 17:23:29 +0100

    Delete Bredex/Bredex directory

--- Bredex/Bredex/job-search-app/src/main/java/com/example/job_search_app/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.job_search_app;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-    Optional<Client> findByApiKey(String apiKey);
-    Optional<Client> findByEmail(String email);
-}
--- Bredex/Bredex/job-search-app/src/main/java/com/example/job_search_app/PositionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.job_search_app;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface PositionRepository extends JpaRepository<Position, Long> {
-    List<Position> findByTitleContainingAndLocationContaining(String title, String location);
-}
